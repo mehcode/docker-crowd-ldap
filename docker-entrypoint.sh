6a7c5fe5a9f14596d6a244c4912c188118bbce67
Ryan Leckey <ryan@launchbadge.com>
2017-04-05 14:01:09 -0700

    Use app envs in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,12 @@
 echo "session.lastvalidation=session.lastvalidation" > ${LDAP_HOME}/crowd.properties \
     && echo "session.tokenkey=session.tokenkey" >> ${LDAP_HOME}/crowd.properties \
     && echo "crowd.server.url=${CROWD_URL}/services/" >> ${LDAP_HOME}/crowd.properties \
-    && echo "application.name=ldap" >> ${LDAP_HOME}/crowd.properties \
+    && echo "application.name=${CROWD_APP_NAME}" >> ${LDAP_HOME}/crowd.properties \
     && echo "http.timeout=30000" >> ${LDAP_HOME}/crowd.properties \
     && echo "session.isauthenticated=session.isauthenticated" >> ${LDAP_HOME}/crowd.properties \
     && echo "application.login.url=${CROWD_URL}" >> ${LDAP_HOME}/crowd.properties \
     && echo "session.validationinterval=0" >> ${LDAP_HOME}/crowd.properties \
-    && echo "application.password=0" >> ${LDAP_HOME}/crowd.properties
+    && echo "application.password=${CROWD_APP_PASSWORD}" >> ${LDAP_HOME}/crowd.properties
 
 # Run
 exec java -cp ${LDAP_HOME} -jar ${LDAP_INSTALL}/target/crowd-ldap-server-1.0.4-SNAPSHOT.jar
